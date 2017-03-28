69cde80138fcf63d7567237614621b0840f27d05
ofk88 <oguzfurkankaytanci@gmail.com>
2017-03-28 22:04:28 +0200

    added AppTunnel.framework

---
+++ AppTunnel.framework/Headers/AppTunnel.h
@@ -0,0 +1,27 @@
+//
+//  AppTunnel.h
+//  AppTunnel
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "ATABTestResult.h"
+#import "ATABTestVariation.h"
+#import "ATFeatureFlagResult.h"
+#import "ATFeatureFlagVariation.h"
+#import "ATNetwork.h"
+
+typedef void (^AppTunnelFetchDoneBlock)(NSArray *abTests, NSArray *featureFlags, NSError *error);
+
+@interface AppTunnel : NSObject
+
++ (void)startAppTunnelWithApiKey:(NSString *)apiKey
+                            projectKey:(NSString *)projectKey
+                     environment:(AppTunnelEnvironment)environment;
+
++ (void)getVariationsForABTests:(NSArray *)abTests
+                   featureFlags:(NSArray *)featureFlags
+                 withFetchBlock:(AppTunnelFetchDoneBlock)fetchDoneBlock;
+
++ (void)setUserId:(NSString *)userId;
+
+@end
